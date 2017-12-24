097792edff2f908111932744b70d12080eee005b
ramp <ramp@kshmac.hsd1.nj.comcast.net>
2017-12-24 12:53:56 -0500

    first commit

---
+++ src/main/java/com/prasads/project1/App.java
@@ -0,0 +1,16 @@
+package com.prasads.project1;
+
+import org.json.JSONArray;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
